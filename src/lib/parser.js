afc4ecd16d0bb2fd21754ffde85f5e59beca4079
Alan Nguyen <nguyen.alan@me.com>
2017-02-24 20:34:07 -0500

    prep for structure testing.

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -1,7 +1,7 @@
 const acorn = require('acorn')
 const walk = require('acorn/dist/walk')
 
-export function markAsVisited (tracker, node) {
+function markAsVisited (tracker, node) {
   tracker[node.type] = true
 }
 
@@ -9,6 +9,8 @@ export function markAsVisited (tracker, node) {
   Required - (String) input: text input to analyze
   Required - (Array) list: array of functionalities to test the input against
   Optional - (Boolean) inclusive: test the input against the list for inclusivity? Default: true
+
+  Output - (Object) Object of the functinoalities and if they passed the given check
 */
 export function passesList (input, list, inclusive = true) {
   if (!list.length) return true // Nothing to match against
@@ -23,4 +25,15 @@ export function passesList (input, list, inclusive = true) {
   parsed = acorn.parse(input)
   walk.simple(parsed, visitSetup)
   return Object.keys(visitedTracker).map((key) => visitedTracker[key]).indexOf(!inclusive) < 0
+}
+
+/*
+  Required - (String) input: text input to analyze
+  Required - (TBD) structure: expected structure of inputs
+
+  Output - (Boolean) Whether or not the input passes the given structure
+*/
+export function passesStructure (input, structure) {
+  if (!structure) return true
+  return false
 }
--- src/lib/parser.test.js
+++ src/lib/parser.test.js
@@ -1,53 +1,64 @@
-import { passesList } from './parser'
+import { passesList, passesStructure } from './parser'
 const acorn = require('acorn')
 
 describe('parser', () => {
   const input = `
-    let a
-    for(var i=5; i<10; ++i) {
-      let b = []
-      if (i===7) {
-        b.push(i)
-      }
-    }`
+  let a
+  for(var i=5; i<10; ++i) {
+    let b = []
+    if (i===7) {
+      b.push(i)
+    }
+  }`
+  describe('#passesList', () => {
 
-  it('throws if there is an error', () => {
-    const badInput = `
+    it('throws if there is an error', () => {
+      const badInput = `
       let a
-      for (
-    `
-    expect(passesList.bind(null, badInput, ['VariableDeclaration'])).toThrow()
+      for (`
+      expect(passesList.bind(null, badInput, ['VariableDeclaration'])).toThrow()
+    })
+
+    describe('whitelisting', () => {
+      it('returns true for empty list', () => {
+        expect(passesList(input, [])).toEqual(true)
+      })
+
+      it('returns true if criteria is met', () => {
+        expect(passesList(input, ['ForStatement'])).toEqual(true)
+      })
+
+      it('returns true if multiple criteria is met', () => {
+        expect(passesList(input, ['ForStatement', 'IfStatement'])).toEqual(true)
+      })
+
+      it('returns false if it does not meet criteria', () => {
+        expect(passesList(input, ['FunctionDeclaration'])).toEqual(false)
+      })
+    })
+
+    describe('blacklisting', () => {
+      it('returns true for empty list', () => {
+        expect(passesList(input, [], false)).toEqual(true)
+      })
+
+      it('returns true if funcationlity is not in blacklist', () => {
+        expect(passesList(input, ['FunctionDeclaration'], false)).toEqual(true)
+      })
+
+      it('returns false if functionality is blacklisted', () => {
+        expect(passesList(input, ['ForStatement'], false)).toEqual(false)
+      })
+    })
   })
-  
-  describe('whitelisting', () => {
-    it('returns true for empty list', () => {
-      expect(passesList(input, [])).toEqual(true)
+
+  describe('#passStructure', () => {
+    it('returns true for no structure expectation', () => {
+      expect(passesStructure(input, null)).toEqual(true)
     })
 
-    it('returns true if criteria is met', () => {
-      expect(passesList(input, ['ForStatement'])).toEqual(true)
-    })
-
-    it('returns true if multiple criteria is met', () => {
-      expect(passesList(input, ['ForStatement', 'IfStatement'])).toEqual(true)
-    })
-
-    it('returns false if it does not meet criteria', () => {
-      expect(passesList(input, ['FunctionDeclaration'])).toEqual(false)
-    })
-  })
-
-  describe('blacklisting', () => {
-    it('returns true for empty list', () => {
-      expect(passesList(input, [], false)).toEqual(true)
-    })
-
-    it('returns true if funcationlity is not in blacklist', () => {
-      expect(passesList(input, ['FunctionDeclaration'], false)).toEqual(true)
-    })
-
-    it('returns false if functionality is blacklisted', () => {
-      expect(passesList(input, ['ForStatement'], false)).toEqual(false)
+    it('returns false if structure is not met', () => {
+      expect(passesStructure(input, {})).toEqual(false)
     })
   })
 })
